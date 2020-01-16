f88eec6efcaba0aa7f0c9ae4c0c7aed91c78f958
thebeastkenji <59965748+thebeastkenji@users.noreply.github.com>
2020-01-16 09:00:38 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
-/* 
-    Author: Kenji Tamane
-    01/16/2020
-    Collaborators: Kenji Tamane, J.P. Jensen
-    Purpose: To print my name
+/**
+*Author: Kenji Tamane
+*01/16/2020
+*Collaborators: Kenji Tamane, J.P. Jensen
+*Purpose: To print my name
 */
 
 #include <stdio.h>
@@ -11,7 +11,7 @@ int main(void) {
     printf("Kenji Tamane\n");
     printf("Computer Science\n");
     printf("J.P. Jensen\n")
-    printf("Computer Science")
+    printf("Computer Science\n")
 
     return 0;
 }
