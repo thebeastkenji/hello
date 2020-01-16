ca15efc30323b444ff55c6d84947de0fb5f8725b
thebeastkenji <59965748+thebeastkenji@users.noreply.github.com>
2020-01-16 08:28:45 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/* 
+    Author: Kenji Tamane
+    01/16/2020
+    Collaborators: Kenji Tamane, JP Jenson
+    Purpose: To print my name
+*/
+
+int main(void) {
+
+    printf("Kenji Tamane\n");
+
+    return 0;
+}
+
