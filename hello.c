38b94b0c54b93692162b2de3f5d70dc01916508f
thebeastkenji <59965748+thebeastkenji@users.noreply.github.com>
2020-01-16 08:51:32 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 /* 
     Author: Kenji Tamane
     01/16/2020
-    Collaborators: Kenji Tamane, J.P Jensen
+    Collaborators: Kenji Tamane, J.P. Jensen
     Purpose: To print my name
 */
 
@@ -9,7 +9,7 @@
 int main(void) {
 
     printf("Kenji Tamane\n");
-    printf("Computer Science\n")
+    printf("Computer Science\n");
     printf("J.P. Jensen\n")
     printf("Computer Science")
 
