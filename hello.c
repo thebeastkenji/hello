d7183763bf025907718b2f5b185583caa790a694
thebeastkenji <59965748+thebeastkenji@users.noreply.github.com>
2020-01-16 08:56:18 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -9,15 +9,9 @@
 int main(void) {
 
     printf("Kenji Tamane\n");
-<<<<<<< HEAD
     printf("Computer Science\n");
     printf("J.P. Jensen\n")
     printf("Computer Science")
-=======
-    printf("Computer Science\n")
-    printf("J.P. Jensen\n");
-    printf("Computer Science");
->>>>>>> d2ea7aa0f09dc74693279988e96ba95ae134418f
 
     return 0;
 }
