77d5fbab73018be26b3e8549fcee68696cc79d63
jjensen654 <jjensen54@huskers.unl.edu>
2020-01-16 08:42:03 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 /* 
     Author: Kenji Tamane
     01/16/2020
-    Collaborators: Kenji Tamane, JP Jenson
+    Collaborators: Kenji Tamane, JP Jensen
     Purpose: To print my name
 */
 
@@ -9,6 +9,8 @@ int main(void) {
 
     printf("Kenji Tamane\n");
     printf("Computer Science\n")
+    printf("J.P. Jensen\n")
+    printf("Computer Science")
 
     return 0;
 }
